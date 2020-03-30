5eb62e5c425109916769a4a7444add25053b6c0b
rainmood42 <62206241+rainmood42@users.noreply.github.com>
2020-03-31 00:28:56 +0900

    Update BookController.java

--- src/main/java/io/lgcns/com/springboot/api/BookController.java
+++ src/main/java/io/lgcns/com/springboot/api/BookController.java
@@ -8,16 +8,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.lgcns.com.springboot.domain.Book;
 import io.lgcns.com.springboot.service.BookService;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
-@RequestMapping("/books")
+@RequestMapping(value = "/books")
 public class BookController {
-	
+
 	@Autowired
 	BookService bookService;
 	
 	@GetMapping("/{bookId}")
-	public ResponseEntity<Book> findById(Long bookId) {
+	public ResponseEntity<Book> findById(@PathVariable Long bookId) {
 		Book book = bookService.findByid(bookId).orElseThrow( () -> new RuntimeException("Not Found: " + bookId) );
 		return ResponseEntity.ok(book);
 	}
